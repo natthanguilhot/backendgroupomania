8011b2aa8a838197b9999596f01b68396550b879
natthanguilhot <72070680+natthanguilhot@users.noreply.github.com>
2022-04-25 13:02:55 +0200

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,22 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const postCtrl = require('../controllers/posts');
-const multer = require('../middleware/multer-posts-config');
-const auth = require('../middleware/auth');
-
+const postCtrl = require("../controllers/posts");
+const multer = require("../middleware/multer-posts-config");
+const auth = require("../middleware/auth");
 
 // Create post
-router.post('/', auth, multer, postCtrl.createPost);
+router.post("/", auth, multer, postCtrl.createPost);
 
 // Get all posts
-router.get('/', postCtrl.getAllPosts);
+router.get("/", postCtrl.getAllPosts);
 
 // Get one post
-router.post('/:id', auth, postCtrl.getOnePosts);
+router.post("/:id", postCtrl.getOnePosts);
 
 // Delete post
-router.delete('/:id', auth, postCtrl.deleteOnePost);
+router.delete("/:id", auth, postCtrl.deleteOnePost);
 
 module.exports = router;
-
